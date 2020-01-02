f55056323e69b7fc558091704eb3ccc1b99bf383
Ilham Ridho Asysyifa'a <45212532+ilhamridho04@users.noreply.github.com>
2020-01-02 16:17:24 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<ilhamridho','mikhmon>|>oZ2amZ5jamJk');
+
+
+
+
+
+
+$data['kenziehotspot'] = array ('1'=>'kenziehotspot!67d207801c76.sn.mynetname.net','kenziehotspot@|@ilhamridho','kenziehotspot#|#oZ2amZ5jamJk','kenziehotspot%Kenzie Mancagar Hotspot','kenziehotspot^www.kenziehotspot.com','kenziehotspot&Rp','kenziehotspot*10','kenziehotspot(1','kenziehotspot)','kenziehotspot=10','kenziehotspot@!@disable');
