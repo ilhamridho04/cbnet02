4ade5262576a38b9eb49261619da956861a85123
Ilham Ridho Asysyifa'a <45212532+ilhamridho04@users.noreply.github.com>
2020-01-03 16:00:08 +0700

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,10 +1,3 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
-$data['mikhmon'] = array ('1'=>'mikhmon<|<ilhamridho','mikhmon>|>oZ2amZ5jamJk');
-
-
-
-
-
-
-$data['kenziehotspot'] = array ('1'=>'kenziehotspot!67d207801c76.sn.mynetname.net','kenziehotspot@|@ilhamridho','kenziehotspot#|#oZ2amZ5jamJk','kenziehotspot%Kenzie Mancagar Hotspot','kenziehotspot^www.kenziehotspot.com','kenziehotspot&Rp','kenziehotspot*10','kenziehotspot(1','kenziehotspot)','kenziehotspot=10','kenziehotspot@!@disable');
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
